b09aff85029f41ec07233289222c035e7121c470
qylibohao <375795423@qq.com>
2016-03-02 18:14:47 +0800

    add equal category

---
+++ UIView+Masonry_EqualSpace/UIView+Masonry_EqualSpace.h
@@ -0,0 +1,16 @@
+//
+//  UIView+EqualSpace.h
+//  Masonry+EqualSpace
+//
+//  Created by libohao on 16/3/2.
+//  Copyright © 2016年 libohao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Masonry_EqualSpace)
+
+- (void)equalHorizontalSpacingWith:(NSArray*)views;
+- (void)equalVerticalSpacingWith:(NSArray*)views;
+
+@end
