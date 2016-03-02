1d09a1884b5204134bcd68f41ac7ee27300696a1
qylibohao <375795423@qq.com>
2016-03-02 20:28:10 +0800

    add screen file

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,12 +6,6 @@
 // project.
 
 
-// MMPlaceHolder
-#define COCOAPODS_POD_AVAILABLE_MMPlaceHolder
-#define COCOAPODS_VERSION_MAJOR_MMPlaceHolder 1
-#define COCOAPODS_VERSION_MINOR_MMPlaceHolder 9
-#define COCOAPODS_VERSION_PATCH_MMPlaceHolder 0
-
 // Masonry
 #define COCOAPODS_POD_AVAILABLE_Masonry
 #define COCOAPODS_VERSION_MAJOR_Masonry 0
